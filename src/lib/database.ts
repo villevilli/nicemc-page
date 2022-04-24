16cc5db7afe5a32a2ce31ce223427cbbd6b1e9d7
Villsukka <kujala.ville.vk@gmail.com>
2022-04-24 10:12:52 +0300

    Adds a user and token schema and links all the schemas together

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { DateTime } from 'luxon';
 
 const dbUrl = import.meta.env.VITE_MONGO_URL
 
@@ -10,8 +11,8 @@ mongoose.connect(dbUrl, {
 
 const articleSchema = new mongoose.Schema({
     address: { type: String, unique: true },
-    title: String,
-    author: String,
+    title: { type: String, index: true },
+    articles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     article: String,
     description: String,
     public: { type: Boolean, default: false},
@@ -20,4 +21,31 @@ const articleSchema = new mongoose.Schema({
     timestamps: true
 })
 
+const userSchema = new mongoose.Schema({
+    publicName: String,
+    username: String,
+    password: String,
+    email: String,
+    permissions: {
+        administrator: Boolean,
+        publishArticles: Boolean,
+        createArticles: Boolean,
+        publishAllArticles: Boolean,
+        modifyAllArticles: Boolean,
+    },
+    articles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article'}]
+})
+
+const tokenSchema = new mongoose.Schema({
+    token: String,
+    expiry: {type: Date, default: DateTime.now().plus({ days: 7 })},
+    user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+})
+
 export const Article = mongoose.model('Article', articleSchema)
+export const User = mongoose.model('User', userSchema)
+export const Token = mongoose.model('Token', tokenSchema)
+
+const testToken = new Token({
+    Token: "hello"
+}).save()
